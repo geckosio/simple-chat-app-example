ad3d356b195c68f1ffcace3c9a540f4294289021
yandeu <20306025+yandeu@users.noreply.github.com>
2019-11-20 00:17:40 +0100

    Set custom port and url

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const geckos = require('@geckos.io/server').default
 const { iceServers } = require('@geckos.io/server')
 
 const io = geckos({
-  ordered: true,
-  maxRetransmits: 10,
+  ordered: true, // in a game, this should be set to false (default is false)
+  maxRetransmits: 10, // in a game, this should be set to 0 (default is 0)
   iceServers: process.env.NODE_ENV === 'production' ? iceServers : []
 })
 
-io.listen()
+// listen on port 3000 (default is 9208)
+io.listen(3000)
 
 io.onConnection(channel => {
   channel.onDisconnect(() => {
